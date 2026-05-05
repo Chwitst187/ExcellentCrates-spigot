af2fbe6dde0362d39a4759cc76ddfb6310749d3b
Chris <90093334+Chwitst187@users.noreply.github.com>
2026-05-05 19:27:26 +0200

    Fix Folia startup crash in data task scheduling

--- src/main/java/su/nightexpress/excellentcrates/data/DataManager.java
+++ src/main/java/su/nightexpress/excellentcrates/data/DataManager.java
@@ -107,7 +107,7 @@ public class DataManager extends AbstractManager<CratesPlugin> {
             this.plugin.runTaskAsync(task -> action.run());
         }
         catch (UnsupportedOperationException ignored) {
-            this.plugin.runTask(task -> action.run());
+            action.run();
         }
     }
 
